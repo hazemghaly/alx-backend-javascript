11155fdc43556627feb2f2b9bda15d8df68ccbec
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-09 05:04:49 +0200

    Update 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,12 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
   const narray = [];
-  if (array.length >= 0) {
-    for (let idx = 0; idx < array.length;) {
+  for (let idx = 0; idx < array.length;) {
       idx += 1;
       const value = array[idx];
       narray.push(appendString + value);
     }
-  }
   return narray;
 }
 
