c92372331860596ee20bea163a208570f65a3e4a
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-09 19:53:01 +0200

    Update 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,10 +1,12 @@
 export default function createReportObject(employeesList) {
   const allEmployees = {};
-  for (const departmentName in employeesList) {
-    if (employeesList.hasOwnProperty(departmentName)) {
-      allEmployees[departmentName] = Array.isArray(employeesList[departmentName]) ? employeesList[departmentName] : [];
+  departmentNames.forEach((departmentName) => {
+    const department = employeesList[departmentName];
+    if (!Array.isArray(allEmployees[departmentName])) {
+      allEmployees[departmentName] = [];
     }
-  }
+    allEmployees[departmentName].push(department.name);
+  });
   const H = {
     allEmployees,
     getNumberOfDepartments() {
