0d276e88bb1857d3da8f93d5fe845f7622c7e827
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-09 04:40:49 +0200

    Update 9-getFullBudget.js

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import getBudgetObject from './7-getBudgetObject.js';
-
-export default function getFullBudgetObject(income, gdp, capita) {
-  const budget = getBudgetObject(income, gdp, capita);
-  const fullBudget = {
-    ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
-    },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
-    },
-  };
-  return fullBudget;
-}
-=======
 import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
@@ -30,4 +13,3 @@ export default function getFullBudgetObject(income, gdp, capita) {
   };
   return fullBudget;
 }
->>>>>>> f1c069b245e9adce35076ac11dccfd46f8c64968
