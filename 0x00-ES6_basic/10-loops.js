d49a2dd1da1219a95094497aec8d3fa46ae5e2c9
hazemghaly <hazemghaly30@gmail.com>
2024-03-09 03:09:17 +0000

    0x00-ES6_basic

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
   const narray = [];
   for (let idx = 0; idx < array.length;) {
-      idx += 1;
-      const value = array[idx];
-      narray.push(appendString + value);
-    }
+    idx += 1;
+    const value = array[idx];
+    narray.push(appendString + value);
+  }
   return narray;
 }
-
