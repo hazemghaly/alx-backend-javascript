fd94687505e3a6274b10a47efc53cac455ea1caa
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-11 01:45:23 +0200

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,20 @@
+// import { createUser, uploadPhoto } from './utils';
+
+// export default async function handleProfileSignup() {
+//   try {
+//     const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+//     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+//   } catch (Error) {
+//     console.log(Error('Signup system offline'));
+//   }
+// }
 import { createUser, uploadPhoto } from './utils';
 
 export default async function handleProfileSignup() {
-  try {
-    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+  const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+  if (photo && user) {
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-  } catch (Error) {
+  } else {
     console.log(Error('Signup system offline'));
   }
 }
