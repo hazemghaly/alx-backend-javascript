3eeab64a4f2ee40aef2556721e111472cbf1383e
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-10 23:50:56 +0200

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,12 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    promise
-      .then(() => {
-        resolve({ status: 200, body: 'Success' });
-      })
-      .catch(() => {
-        reject(Error());
-      })
-      .finally(() => {
-        console.log('Got a response from the API');
-      });
-  });
+  return promise
+    .then(() => {
+      return ({ status: 200, body: 'Success' });
+    })
+    .catch(() => {
+      return (Error());
+    })
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
