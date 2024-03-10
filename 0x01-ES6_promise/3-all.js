9ea10f3bfd0bc7e29eedb51d6b280232da5c8d42
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-11 01:52:06 +0200

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -11,10 +11,10 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default async function handleProfileSignup() {
-  const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-  if (photo && user) {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-  } else {
-    console.log(Error('Signup system offline'));
+  } catch (e) {
+    console.error(Error('Signup system offline'));
   }
 }
