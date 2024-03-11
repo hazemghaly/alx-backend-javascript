18d595a902f4409481f385baa58a5676af19601e
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-11 04:41:12 +0200

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,8 +3,11 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup() {
   try {
-    await Promise.all([uploadPhoto(), signUpUser()]);
-    return [Promise.all.status, Promise.all.value];
+    const [photo, user] = await Promise.all([uploadPhoto(), signUpUser()]);
+    return [
+      { status: 'fulfilled', value: user },
+      { status: 'fulfilled', value: photo },
+    ];
   } catch (e) {
     return [{ status: 'rejected', value: Error.toString() }];
   }
