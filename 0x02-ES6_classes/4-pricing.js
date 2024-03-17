a4f9d7d3c5bff319b03b5ba9451c8fafb66da024
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-17 06:31:20 +0200

    Add files via upload

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,39 +1,31 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-  static conversionRate = 0;
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
-  }
-
-  get amount() {
-    return this._amount;
-  }
-
-  set amount(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('amount must be a number');
-    }
-    this._amount = value;
-  }
-
-  get currency() {
-    return this._currency;
-  }
-
-  set currency(value) {
-    if (!(value instanceof Currency)) {
-      throw new TypeError('currency must be a string');
-    }
-    this._currency = Currency;
+    this.conversionRate = 0;
+    Object.defineProperties(this, {
+      currency: {
+        get: () => this._currency,
+        set: () => {
+          this._currency = Currency;
+        },
+      },
+      amount: {
+        get: () => this._amount,
+        set: (value) => {
+          this._amount = value;
+        },
+      },
+    });
   }
 
   displayFullPrice() {
     return `${this._amount} ${this.currency._name} (${this.currency._code})`;
   }
 
-  static convertPrice(){
-      return `${Pricing._amount} * ${this.conversionRate}`;
+  static convertPrice() {
+    return `${Pricing._amount} * ${this.conversionRate}`;
   }
 }
