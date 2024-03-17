615c6ba090c82178b2d0f8374339eafe239d68e7
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-17 08:42:40 +0200

    Add files via upload

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -10,7 +10,5 @@ export default class Building {
     return this._sqft;
   }
 
-  static evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
+  evacuationWarningMessage() {}
 }
