b611a03fd248afadb9bff7b9a719401d70288971
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-17 07:53:26 +0200

    Add files via upload

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,9 +1,16 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = typeof sqft === 'number' ? sqft : console.log(TypeError('sqft must be a number'));
+    this._sqft = sqft;
+    if (this.constructor === Building) {
+      console.log(Error('Class extending Building must override evacuationWarningMessage'));
+    }
   }
 
-  evacuationWarningMessage() {
+  get sqft() {
+    return this._sqft;
+  }
+
+  static evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
