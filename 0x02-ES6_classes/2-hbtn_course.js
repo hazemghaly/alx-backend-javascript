d7c8738467f17f23820232750a323c0e1ad77bb2
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-03-17 03:15:45 +0200

    Add files via upload

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -7,18 +7,27 @@ export default class HolbertonCourse {
       length: {
         get: () => this._length,
         set: (value) => {
+          if (typeof value !== 'Number') {
+            throw new TypeError('Length must be a number');
+          }
           this._length = value;
         },
       },
       name: {
         get: () => this._name,
         set: (value) => {
+          if (typeof value !== 'string') {
+            throw new TypeError('Name must be a string');
+          }
           this._name = value;
         },
       },
       students: {
         get: () => this._students,
         set: (value) => {
+          if (typeof value !== 'array') {
+            throw new TypeError('students must be a array');
+          }
           this._students = value;
         },
       },
