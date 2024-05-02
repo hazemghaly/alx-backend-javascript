92b201f683583a2583814f25852ff11c734ebbaf
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-03 02:35:49 +0300

    Add files via upload

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
