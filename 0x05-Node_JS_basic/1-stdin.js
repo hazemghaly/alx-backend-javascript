5ab67b5fbe06b705f7da5a094db2b2cb3816b3e4
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-02 04:46:48 +0300

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,8 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data}`);
+  const name = data.toString().trim(); // Remove newline character
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 
 process.stdin.on('close', () => {
