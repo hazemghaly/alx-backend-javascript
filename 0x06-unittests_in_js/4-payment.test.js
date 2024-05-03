a9b36590eb310d3eb4dd53b6701b5166ea5f58a3
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-03 05:49:36 +0300

    Add files via upload

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,15 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  it(' uses the calcNum method of Utils', () => {
+    const h = sinon.spy(Utils);
+
+    sendPaymentRequestToApi(100, 20);
+    expect(h.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(h.calculateNumber.callCount).to.be.equal(1);
+    h.calculateNumber.restore();
+  });
+});
